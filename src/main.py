1673b920f39e4a515a6cb42809528d419babec34
PlatovD <platovdima6@gmail.com>
2025-08-28 12:58:53 +0300

    lifespan for main method

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+from contextlib import asynccontextmanager
+from typing import AsyncIterator, Dict, Any
+
 from fastapi import FastAPI
 from starlette.config import environ
 from telebot import TeleBot
@@ -21,47 +24,56 @@ def create_configured_bot() -> TeleBot:
     return bot_ex
 
 
-# bot = create_configured_bot()
-app = FastAPI()
+logger = set_up_logger('log/main.log')
 
-# initialize connections
-RedisConnector.init_connection()
-DBConnector.init_connection()
 
-# DI connections into data change classes
-db = DataBase(connection=DBConnector.get_connection())
-redis_db = RedisDatabase(redis_init=RedisConnector.get_connection())
+@asynccontextmanager
+async def lifespan(app_fastAPI: FastAPI) -> AsyncIterator[Dict[str, Any]]:
+    try:
+        # initialize connections
+        RedisConnector.init_connection()
+        DBConnector.init_connection()
 
-# starting updaters (need to create resetter for users_per_day)
-SchedulerWrapper.set_redis_db(redis_db)
-SchedulerWrapper.init()
-SchedulerWrapper.on_start()
+        # DI connections into data change classes
+        db = DataBase(connection=DBConnector.get_connection())
+        redis_db = RedisDatabase(redis_init=RedisConnector.get_connection())
 
-# start parser service
-parser_service = ParserService(parser=ScheduleParser(environ.get('MAIN_SCHEDULE_PATH')), redis=redis_db)
+        # starting updaters (need to create resetter for users_per_day)
+        SchedulerWrapper.set_redis_db(redis_db)
+        SchedulerWrapper.init()
+        SchedulerWrapper.on_start()
 
-# starting auth service
-authentication_service = AuthenticationService(database=db, redis_init=redis_db)
+        # start parser service
+        parser_service = ParserService(parser=ScheduleParser(environ.get('MAIN_SCHEDULE_PATH')), redis=redis_db)
 
-bot = create_configured_bot()
+        # starting auth service
+        authentication_service = AuthenticationService(database=db, redis_init=redis_db)
 
-# setting handlers for bot
-bot_controller = BotController(
-    bot=bot,
-    parser_service=parser_service,
-    authentication_service=authentication_service,
-    redis_db=redis_db,
-    logger=set_up_logger('./log/bot_controller.log')
-)
-bot_controller.start_controller()
+        bot = create_configured_bot()
 
-# setting handlers for api
-api_controller = APIController(
-    bot=bot,
-    parser_service=parser_service,
-    authentication_service=authentication_service,
-    redis_db=redis_db,
-    logger=set_up_logger('./log/api_controller.log'),
-)
-api_controller.start_controller()
-app.include_router(api_controller.get_router())
+        # setting handlers for bot
+        bot_controller = BotController(
+            bot=bot,
+            parser_service=parser_service,
+            authentication_service=authentication_service,
+            redis_db=redis_db,
+            logger=set_up_logger('./log/bot_controller.log')
+        )
+        bot_controller.start_controller()
+
+        # setting handlers for api
+        api_controller = APIController(
+            bot=bot,
+            parser_service=parser_service,
+            authentication_service=authentication_service,
+            redis_db=redis_db,
+            logger=set_up_logger('./log/api_controller.log'),
+        )
+        api_controller.start_controller()
+        app_fastAPI.include_router(api_controller.get_router())
+        yield {}
+    except Exception as err:
+        logger.error("Error during running application. %s", err)
+
+
+app = FastAPI(lifespan=lifespan)
