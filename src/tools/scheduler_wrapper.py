5a1365989b90f39d02f77bc9e714e2e7a653a684
PlatovD <platovdima6@gmail.com>
2025-08-31 17:23:10 +0300

    Background scheduler udpates to stability

--- src/tools/scheduler_wrapper.py
+++ src/tools/scheduler_wrapper.py
@@ -2,6 +2,8 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 from src.parser_service.excell_loader import download_and_update
 from src.authentication_service.db.redis_repo import RedisDatabase
+from pytz import utc
+import threading
 
 
 class SchedulerWrapper:
@@ -14,7 +16,8 @@ class SchedulerWrapper:
 
     @classmethod
     def init(cls):
-        cls.__scheduler.add_job(download_and_update, 'interval', minutes=30)
+        cls.__scheduler.configure(timezone=utc)
+        cls.__scheduler.add_job(download_and_update, 'cron', hour='7-22', minute='*/20', second='5')
         cls.__scheduler.add_job(cls.__redis_db.change_week_type, 'cron', day_of_week=5, hour=20, minute=0, second=0)
 
     @classmethod
